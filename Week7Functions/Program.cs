33ea398f4e5deab18ed018a63c8409fa27404f25
Tonu Riisimae <example@mail.com>
2024-12-11 16:57:40 +0200

    Week7

---
+++ Week7Functions/Program.cs
@@ -0,0 +1,14 @@
+﻿string hello = "Hello, world!";
+string panic = "Don't panic!";
+
+PrintStringFiveTimes(hello);
+PrintStringFiveTimes(panic);
+PrintStringFiveTimes("Tõnu");
+
+static void PrintStringFiveTimes(string someString)
+{
+    for (int i = 0; i < 5; i++)
+    {
+        Console.WriteLine(someString);
+    }
+}
